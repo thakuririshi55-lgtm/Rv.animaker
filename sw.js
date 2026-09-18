1798889d7e2c0237c469844cfec90ced4034056b
thakuririshi55-lgtm <thakuririshi55@gmail.com>
2026-09-18 08:16:03 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,38 @@
 const CACHE_NAME = "aniforge-v1";
 
 const FILES = [
-    "./",
-    "./index.html",
-    "./manifest.json"
+  "./",
+  "./index.html",
+  "./manifest.json"
 ];
 
+// Install
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(FILES))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES);
+    })
+  );
 });
 
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(response => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
